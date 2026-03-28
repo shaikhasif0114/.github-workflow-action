968f794f49833520f8b57cfadb23802bf47225b3
Asif Shaikh <shaikhasif0114@gmail.com>
2026-03-28 22:48:18 +0530

    updated all files

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -12,6 +11,4 @@ def hello_world():
 def health():
     return 'Server is up and running'
 
-if __name__ == "__main__":
-app.run(debug=TRUE, host='0.0.0.0', ports=80)
-
+app.run(debug=True, host='0.0.0.0', ports=80)
