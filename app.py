bfbfe2af61d393f7c0ca3523e8c9243205c11026
Asif Shaikh <shaikhasif0114@gmail.com>
2026-04-01 10:24:19 +0530

    added updated yml

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def health():
     return 'Server is up and running'
 
 if __name__ == '__main__':                         # ✅ outside functions
-    app.run(debug=True, host='0.0.0.0', port=80)   # ✅ correct place
+    app.run(port=80)   # ✅ correct place
