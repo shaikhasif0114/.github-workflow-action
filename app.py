852cb9a0326a89a6c3b30d18971a3af582a4a06d
Asif Shaikh <shaikhasif0114@gmail.com>
2026-04-01 10:47:58 +0530

    added updated yml

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
   
 app = Flask(__name__)
   
-  @app.route('/')
-  def hello_world():
+@app.route('/')
+def hello_world():
     return render_template('index.html')
   
-  @app.route('/health')
-  def health():
+@app.route('/health')
+def health():
     return 'Server is up and running'
   
-  if __name__ == '__main__':                         # ✅ outside functions
-    app.run(port=80)   # ✅ correct place
+app.run(port=80)   # ✅ correct place
+    
